4e744db4e46b1ac5cd6b39f80dba7c01627bd0c0
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-05-06 15:13:39 +0500

    media query added

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,10 +4,10 @@ const Result = ({option,selected}) => {
   return (
         <>
         {option==='eng'?<div className="result">
-          <h2 className="result_word">{selected.word_eng}</h2>
+          <h3 className="result_word">{selected.word_eng}</h3>
           <p className="result_definition">{selected.definition_eng}</p>
       </div>:<div className="result">
-          <h2 className="result_word">{selected.word_uz}</h2>
+          <h3 className="result_word">{selected.word_uz}</h3>
           <p className="result_definition">{selected.definition_uz}</p>
       </div>}
       </>
